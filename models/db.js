ec62980cb570a6b4196177b86ac39e4a40640a9a
Douglas Walter <doug@kosmojo.com>
2016-06-28 12:39:08 -0600

    Update db.js

--- models/db.js
+++ models/db.js
@@ -5,7 +5,7 @@ var options = {
  server:  { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
 };  
-var mongodbUri = process.env.MONGOLAB_URI || "mongodb://localhost/YOURDATABASE";
+var mongodbUri = process.env.MONGODB_URI || "mongodb://localhost/YOURDATABASE";
 var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 
 mongoose.connect(mongooseUri, options);
